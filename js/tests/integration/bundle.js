09b1c38a3d2a137c17759c5642b85c3b5dcd5d59
did1335 <did1335@gmail.com>
2019-09-16 22:53:26 +0800

    recovery

--- js/tests/integration/bundle.js
+++ js/tests/integration/bundle.js
@@ -1,6 +1,5 @@
-import '../../../node_modules/popper.js/dist/umd/popper.min.js';
-
-import { Tooltip } from '../../../dist/js/bootstrap.esm.js';
+import 'popper.js'
+import { Tooltip } from '../../../dist/js/bootstrap.esm.js'
 
 window.addEventListener('load', () => {
   [...document.querySelectorAll('[data-toggle="tooltip"]')]
